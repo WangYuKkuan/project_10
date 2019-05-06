19a0f142f3359e8b2343371ed8d3194c1558a16a
WangYuKkuan <810312736@qq.com>
2019-05-06 15:51:22 +0800

    修改了东西

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -13,7 +13,7 @@ class TestLogin:
         assert 1
     def test_login4(self):
         print("test_login4")
-        assert 1
+        assert 0
     def test_login5(self):
         print("test_login5")
         assert 1
